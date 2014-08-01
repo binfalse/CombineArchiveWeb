f0b1863873883e0bd6276c9ff4f6eb6259dc323b
mp487 <martin.peters3@uni-rostock.de>
2014-08-01 12:00:20 +0200

    test permission [see #1]

--- src/main/java/de/unirostock/sems/cbarchive/web/Tools.java
+++ src/main/java/de/unirostock/sems/cbarchive/web/Tools.java
@@ -14,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.Part;
-import javax.ws.rs.core.NewCookie;
 import javax.xml.bind.DatatypeConverter;
 
 import de.binfalse.bflog.LOGGER;

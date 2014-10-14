547102b26a243330610f71102214b73d3693ae7e
mp487 <martin.peters3@uni-rostock.de>
2014-10-14 14:02:52 +0200

    rebased

--- src/main/java/de/unirostock/sems/cbarchive/web/rest/ShareApi.java
+++ src/main/java/de/unirostock/sems/cbarchive/web/rest/ShareApi.java
@@ -1,9 +1,11 @@
 package de.unirostock.sems.cbarchive.web.rest;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.text.ParseException;
+import java.util.Arrays;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.CookieParam;
@@ -18,17 +20,22 @@ import javax.ws.rs.core.NewCookie;
 import javax.ws.rs.core.Response;
 import javax.xml.transform.TransformerException;
 
+import org.apache.commons.io.FileUtils;
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpHead;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.jdom2.JDOMException;
 
 import de.binfalse.bflog.LOGGER;
 import de.unirostock.sems.cbarchive.CombineArchiveException;
 import de.unirostock.sems.cbarchive.web.Fields;
 import de.unirostock.sems.cbarchive.web.HttpImporter;
-<<<<<<< Upstream, based on master
-import de.unirostock.sems.cbarchive.web.ImporterException;
-=======
 import de.unirostock.sems.cbarchive.web.Tools;
->>>>>>> 19bc7df moved Exception classes into own package
 import de.unirostock.sems.cbarchive.web.UserManager;
 import de.unirostock.sems.cbarchive.web.WorkspaceManager;
 import de.unirostock.sems.cbarchive.web.dataholder.Workspace;
@@ -104,7 +111,7 @@ public class ShareApi extends RestHelper {
 	}
 	
 	@GET
-	@Path("/import")
+	@Path("/share")
 	@Produces( MediaType.TEXT_PLAIN )
 	public Response downloadRemoteArchive(@CookieParam(Fields.COOKIE_PATH) String userPath, @Context HttpServletRequest requestContext, @QueryParam("remote") String remoteUrl, @QueryParam("name") String archiveName) {
 		// user stuff
